09febdc2ca3fb833cfc3301499c201e6a1ded85c
Spen Hanley <b00330712@studentmail.uws.ac.uk>
2017-12-03 05:16:59 +0000

    This addition should allow autocreation of database tables

---
+++ app/database.py
@@ -0,0 +1,17 @@
+from sql_alchemy import create_engine
+from sql_alchemy.orm import scoped_session, sessionmaker
+from sql_alchemy.ext.declarative import declarative_base
+
+engine = create_engine('mysql+pymysql://root:root@localhost/fforum_db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+	# Import all modules here that might define models so that
+	# they will be registered properly on the metadata. Otherwise
+	# you will have to import them first before calling init_db()
+
+	import app.models
+	Base.metadata.create_all(bind=engine)
