5b283205c68fdce6c430b3bc9c5d461105e1ef3d
Spen Hanley <b00330712@studentmail.uws.ac.uk>
2018-02-03 13:01:53 +0000

    Removed unecessary files

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install python3-pip
-sudo -H pip3 install virtualenv
-cd /vagrant
-source flask_env/bin/activate/
-pip install -r requirements.txt
